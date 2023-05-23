edbc0c38b2a80bdf7e8f8f83dc3964dab67d8e47
ranunclulus <morion002@gmail.com>
2023-05-23 15:47:36 +0900

    fix: connection 분리

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -2,16 +2,16 @@ package com.example.tobyspring3.dao;
 
 import com.example.tobyspring3.domain.User;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Map;
 
 import static java.lang.System.getenv;
+import static java.lang.System.setOut;
+
 
 public class UserDao {
-    public void add(User user) throws ClassNotFoundException, SQLException {
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         Map<String, String> env = getenv();
         String dbHost = env.get("DB_HOST");
         String dbUser = env.get("DB_USER");
@@ -21,6 +21,11 @@ public class UserDao {
         Connection conn = DriverManager.getConnection(
                 dbHost, dbUser, dbPassword
         );
+        return conn;
+    }
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection conn = getConnection();
+
         PreparedStatement pstmt = conn.prepareStatement("insert into User(id, name, password) values(?, ?, ?)");
         pstmt.setString(1, user.getId());
         pstmt.setString(2, user.getName());
@@ -32,6 +37,27 @@ public class UserDao {
 
     }
 
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection conn = getConnection();
+
+        PreparedStatement pstmt = conn.prepareStatement("select id, name, password from User where id = ?");
+        pstmt.setString(1, id);
+
+        ResultSet rs = pstmt.executeQuery(); // 내가 요청한 결과값이 rs에 담아옴
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        // 안 닫아 주면 계속 연결돼 있음 -> 연결만 늘어나서 서비스 장애가 생길 수 있음
+        pstmt.close();
+        conn.close();
+        rs.close();
+        return user;
+    }
+
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
@@ -39,6 +65,9 @@ public class UserDao {
         user.setId("2");
         user.setName("huisu");
         user.setPassword("2459810");
-        userDao.add(user);
+        User selectuser = userDao.get("1");
+        System.out.println(selectuser.getId());;
+        System.out.println(selectuser.getName());
+        System.out.println(selectuser.getPassword());
     }
 }
